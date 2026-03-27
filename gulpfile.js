3fb94b4f404e80badfd2c1073faa75d8d636ead4
Maria Luiza <m.luiza0020@gmail.com>
2026-03-27 01:15:52 -0300

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 //scss-css comprimido
 function styles() {
@@ -17,8 +24,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images); //executa a funcão styles
+exports.default = gulp.parallel(styles, images, scripts); //executa a funcão styles
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
